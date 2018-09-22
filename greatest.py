ca5b33d022bfae6125ad871c856560a34c5dac17
mariswethz <43032088+mariswethz@users.noreply.github.com>
2018-09-22 09:58:37 +0530

    Add files via upload

---
+++ greatest.py
@@ -0,0 +1,9 @@
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+if a>b and a>c:
+	print("a is greatest")
+elif b>c:
+	print("b is the greatest")
+else:
+	print("c is the greatest")
---
+++ lab 5.py
@@ -0,0 +1,502 @@
+a is an odd number
+1
+a is an even number
+2
+a is an odd number
+3
+a is an even number
+4
+a is an odd number
+5
+a is an even number
+6
+a is an odd number
+7
+a is an even number
+8
+a is an odd number
+9
+a is an even number
+10
+a is an odd number
+11
+a is an even number
+12
+a is an odd number
+13
+a is an even number
+14
+a is an odd number
+15
+a is an even number
+16
+a is an odd number
+17
+a is an even number
+18
+a is an odd number
+19
+a is an even number
+20
+a is an odd number
+21
+a is an even number
+22
+a is an odd number
+23
+a is an even number
+24
+a is an odd number
+25
+a is an even number
+26
+a is an odd number
+27
+a is an even number
+28
+a is an odd number
+29
+a is an even number
+30
+a is an odd number
+31
+a is an even number
+32
+a is an odd number
+33
+a is an even number
+34
+a is an odd number
+35
+a is an even number
+36
+a is an odd number
+37
+a is an even number
+38
+a is an odd number
+39
+a is an even number
+40
+a is an odd number
+41
+a is an even number
+42
+a is an odd number
+43
+a is an even number
+44
+a is an odd number
+45
+a is an even number
+46
+a is an odd number
+47
+a is an even number
+48
+a is an odd number
+49
+a is an even number
+50
+a is an odd number
+51
+a is an even number
+52
+a is an odd number
+53
+a is an even number
+54
+a is an odd number
+55
+a is an even number
+56
+a is an odd number
+57
+a is an even number
+58
+a is an odd number
+59
+a is an even number
+60
+a is an odd number
+61
+a is an even number
+62
+a is an odd number
+63
+a is an even number
+64
+a is an odd number
+65
+a is an even number
+66
+a is an odd number
+67
+a is an even number
+68
+a is an odd number
+69
+a is an even number
+70
+a is an odd number
+71
+a is an even number
+72
+a is an odd number
+73
+a is an even number
+74
+a is an odd number
+75
+a is an even number
+76
+a is an odd number
+77
+a is an even number
+78
+a is an odd number
+79
+a is an even number
+80
+a is an odd number
+81
+a is an even number
+82
+a is an odd number
+83
+a is an even number
+84
+a is an odd number
+85
+a is an even number
+86
+a is an odd number
+87
+a is an even number
+88
+a is an odd number
+89
+a is an even number
+90
+a is an odd number
+91
+a is an even number
+92
+a is an odd number
+93
+a is an even number
+94
+a is an odd number
+95
+a is an even number
+96
+a is an odd number
+97
+a is an even number
+98
+a is an odd number
+99
+a is an even number
+100
+a is an odd number
+101
+a is an even number
+102
+a is an odd number
+103
+a is an even number
+104
+a is an odd number
+105
+a is an even number
+106
+a is an odd number
+107
+a is an even number
+108
+a is an odd number
+109
+a is an even number
+110
+a is an odd number
+111
+a is an even number
+112
+a is an odd number
+113
+a is an even number
+114
+a is an odd number
+115
+a is an even number
+116
+a is an odd number
+117
+a is an even number
+118
+a is an odd number
+119
+a is an even number
+120
+a is an odd number
+121
+a is an even number
+122
+a is an odd number
+123
+a is an even number
+124
+a is an odd number
+125
+a is an even number
+126
+a is an odd number
+127
+a is an even number
+128
+a is an odd number
+129
+a is an even number
+130
+a is an odd number
+131
+a is an even number
+132
+a is an odd number
+133
+a is an even number
+134
+a is an odd number
+135
+a is an even number
+136
+a is an odd number
+137
+a is an even number
+138
+a is an odd number
+139
+a is an even number
+140
+a is an odd number
+141
+a is an even number
+142
+a is an odd number
+143
+a is an even number
+144
+a is an odd number
+145
+a is an even number
+146
+a is an odd number
+147
+a is an even number
+148
+a is an odd number
+149
+a is an even number
+150
+a is an odd number
+151
+a is an even number
+152
+a is an odd number
+153
+a is an even number
+154
+a is an odd number
+155
+a is an even number
+156
+a is an odd number
+157
+a is an even number
+158
+a is an odd number
+159
+a is an even number
+160
+a is an odd number
+161
+a is an even number
+162
+a is an odd number
+163
+a is an even number
+164
+a is an odd number
+165
+a is an even number
+166
+a is an odd number
+167
+a is an even number
+168
+a is an odd number
+169
+a is an even number
+170
+a is an odd number
+171
+a is an even number
+172
+a is an odd number
+173
+a is an even number
+174
+a is an odd number
+175
+a is an even number
+176
+a is an odd number
+177
+a is an even number
+178
+a is an odd number
+179
+a is an even number
+180
+a is an odd number
+181
+a is an even number
+182
+a is an odd number
+183
+a is an even number
+184
+a is an odd number
+185
+a is an even number
+186
+a is an odd number
+187
+a is an even number
+188
+a is an odd number
+189
+a is an even number
+190
+a is an odd number
+191
+a is an even number
+192
+a is an odd number
+193
+a is an even number
+194
+a is an odd number
+195
+a is an even number
+196
+a is an odd number
+197
+a is an even number
+198
+a is an odd number
+199
+a is an even number
+200
+a is an odd number
+201
+a is an even number
+202
+a is an odd number
+203
+a is an even number
+204
+a is an odd number
+205
+a is an even number
+206
+a is an odd number
+207
+a is an even number
+208
+a is an odd number
+209
+a is an even number
+210
+a is an odd number
+211
+a is an even number
+212
+a is an odd number
+213
+a is an even number
+214
+a is an odd number
+215
+a is an even number
+216
+a is an odd number
+217
+a is an even number
+218
+a is an odd number
+219
+a is an even number
+220
+a is an odd number
+221
+a is an even number
+222
+a is an odd number
+223
+a is an even number
+224
+a is an odd number
+225
+a is an even number
+226
+a is an odd number
+227
+a is an even number
+228
+a is an odd number
+229
+a is an even number
+230
+a is an odd number
+231
+a is an even number
+232
+a is an odd number
+233
+a is an even number
+234
+a is an odd number
+235
+a is an even number
+236
+a is an odd number
+237
+a is an even number
+238
+a is an odd number
+239
+a is an even number
+240
+a is an odd number
+241
+a is an even number
+242
+a is an odd number
+243
+a is an even number
+244
+a is an odd number
+245
+a is an even number
+246
+a is an odd number
+247
+a is an even number
+248
+a is an odd number
+249
+a is an even number
+250
+a is an odd number
+
